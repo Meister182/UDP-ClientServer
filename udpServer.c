b4a0e26ad6d66a46a4061cdb079c28870a0ad523
Alexandre Marques <c137.marques@gmail.com>
2020-06-11 23:28:17 +0000

    [UDP101] Initial commit

---
+++ udpServer.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+void main(int argc, char **argv){
+
+  int port;
+  char *ipadr;
+  if(argc == 2){
+    port = atoi(argv[1]);
+    ipadr = "INVALID";
+  }else if(argc ==3){
+    port = atoi(argv[1]);
+    ipadr = argv[2];
+  }else{
+    printf("Usage: %s <port>\n", argv[0]);
+    printf("Usage: %s <port> <ip>\n", argv[0]);
+    exit(0);
+  }
+
+
+
+
+  int sockfd;
+  struct sockaddr_in si_me, si_other;
+  char buffer[1024];
+  char clientIP[INET_ADDRSTRLEN];
+  socklen_t addr_size;
+
+  sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+
+  memset(&si_me, '\0', sizeof(si_me));
+  si_me.sin_family = AF_INET;
+  si_me.sin_port = htons(port);
+  if(ipadr != "INVALID"){
+    si_me.sin_addr.s_addr = inet_addr(ipadr);
+  }else{
+    si_me.sin_addr.s_addr = INADDR_ANY;
+  }
+
+  bind(sockfd, (struct sockaddr*)&si_me, sizeof(si_me));
+  addr_size = sizeof(si_other);
+  printf("listening..\n");
+  while(1){
+    recvfrom(sockfd, buffer, 1024, 0, (struct sockaddr*)& si_other, &addr_size);
+    inet_ntop(AF_INET, &si_other.sin_addr, clientIP, sizeof(clientIP));
+    printf("[+]From: %-15s Data: %s", clientIP, buffer);
+  }
+}
